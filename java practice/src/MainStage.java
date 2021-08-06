cf75d71a2d83ca6b7da04c35f1e8fd72f0d2ef28
fairmanbos <88518862+fairmanbos@users.noreply.github.com>
2021-08-06 09:12:30 +0800

    Add files via upload

--- java practice/src/MainStage.java
+++ java practice/src/MainStage.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainStage extends Application {
@@ -12,6 +13,8 @@ public class MainStage extends Application {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
             Scene scene = new Scene(root);
+            Calorie_calculator.setTitle("Calorie Calculator");
+            Calorie_calculator.getIcons().add(new Image("diet_icon.png"));
             Calorie_calculator.setScene(scene);
             Calorie_calculator.show();
         }
